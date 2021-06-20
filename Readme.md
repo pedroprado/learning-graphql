b4d67d960df51b82b7f00f3e01d1e81f48ecfd78
Pedro Prado <pedro.prado@bexsbanco.com.br>
2021-06-20 14:40:15 -0300

    circular resolvers

--- Readme.md
+++ Readme.md
@@ -11,11 +11,22 @@ The more nested data is makes more difficult to:
 * Buildint restullf routing that keeps the restfull conventions leads to **to many http request** for fetching data
 * Overfecthing data: restfull routes fetches all the data when, sometimes, all we need is a single attribute
 
-
 ## What is GraphQL
 
 GraphQL builds **objects relationship** as a graph. So queries to the server is a lot easier do make (we just send the types and attributes we need).
 
-### RootQuerys
+### 1.RootQuerys
 
 Allows the request to get in the applications data graph (this graph might be in database or, in case of a BFF, be constructed by acessing the backed servers).
+
+
+### 2.Resolve Function
+
+"Resolves" the differences from "json" and "type". This means, any key (foreign) is resolved as an object.
+
+### 3.Circulatiry
+
+GraphQL allows circular dependencia (bidirection graphs relationship). The only thing we need to do is to declare the **Fields** of our **Types** as **arrow function**.
+
+### 4.Mutation
+
--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,45 @@
 const graphql = require('graphql');
 const axios = require('axios');
+const { response } = require('express');
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
+
+
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                .then(response => response.data)
+            }
+        }
+    }),
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields:{
+    fields:() => ({
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
         age: {type: GraphQLInt},
-    },   
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then(response => response.data)
+            }
+        }
+    }),   
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -24,8 +50,16 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString}},
             resolve(parentValue, args){
                 return axios.get(`http://localhost:3000/users/${args.id}`)
-                .then(response => response.data);
+                .then(response => response.data)
             },
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString}},
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                .then(response => response.data)
+            }
         }
     },
 });
